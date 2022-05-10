082f56140de5efdac398710174badea8eae0939b
ANDERSON ANTONIO PIRES DE ALMEIDA <33177014+andersonpiresdc@users.noreply.github.com>
2022-05-10 18:57:54 -0300

    Projeto do Curso da UDEMY, react + firebird

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from './firebaseConnection';
+import { useState, useEffect } from 'react';
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [titulo, setTitulo] = useState('');
+   const [autor, setAutor] = useState('');
+   const [posts, setPosts] = useState([]);
+
+   useEffect(() => {
+      async function loadPosts(){
+         await firebase.firestore().collection('posts')
+         .onSnapshot((doc) =>{
+            let meusposts = [];
+            doc.forEach((item)=>{
+               meusposts.push({
+                  id: item.id,
+                  titulo: item.data().titulo,
+                  autor: item.data().autor
+               })
+            })
+         setPosts(meusposts);
+         })
+      }
+
+      loadPosts();
+
+   }, []);
+
+   async function handleAdd(){
+      await firebase.firestore().collection('posts')
+      .add({
+         titulo: titulo,
+         autor: autor   
+      })
+      .then(() => {
+         console.log('Dados cadastrado com sucesso!');
+         setTitulo('');
+         setAutor('');
+      })
+      .catch((error)=>{
+         console.log('Gerou algum erro: '+error);
+      })
+   }
+
+   async function buscarPost(){
+      // await firebase.firestore().collection('posts')
+      // .doc('1234')
+      // .get()
+      // .then((snapshot)=>{
+      //    setTitulo(snapshot.data().titulo);
+      //    setAutor(snapshot.data().autor);
+      // })
+      // .catch((error) => {
+      //    console.log("Deu algum erro!");
+      // })
+      await firebase.firestore().collection('posts')
+      .get()
+      .then((snapshot)=>{
+         let lista = [];
+         snapshot.forEach((doc)=>{
+            lista.push({
+               id: doc.id,
+               titulo: doc.data().titulo,
+               autor: doc.data().autor
+            })
+         setPosts(lista);
+         })
+      })
+      .catch((error)=>{
+         console.log("Deu algum erro ao carregar os Posts")
+      })
+   }
+
+   return (
+      <div className="container">
+         <h1>ReactJS + Firebase</h1><br/>
+         <label>Titulo:</label>
+         <textarea type="text" value={titulo} onChange={ (e) => setTitulo(e.target.value)}/>
+         <label>Autor:</label>
+         <textarea type="text" value={autor} onChange={ (e) => setAutor(e.target.value)}/>
+         <button onClick={handleAdd}>Cadastrar</button>
+         <button onClick={buscarPost}>Buscar Post</button><br/>
+
+         <ul>
+            {posts.map((post)=>{
+               return(
+                  <li key={post.id}>
+                     <span>Titulo: {post.titulo}</span><br/>
+                     <span>Autor: {post.autor}</span><br/><br/>
+                  </li>
+               )
+            })}
+         </ul>
+
+
+      </div>
+   );
 }
 
 export default App;
---
+++ src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+// import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeC5Ywc7hnc0nUn3YZpUemXfFFSpeitgs",
+  authDomain: "cursoreactjs-2deae.firebaseapp.com",
+  projectId: "cursoreactjs-2deae",
+  storageBucket: "cursoreactjs-2deae.appspot.com",
+  messagingSenderId: "979631654438",
+  appId: "1:979631654438:web:0ce32d676074d4432b08e7",
+  measurementId: "G-6KYLMT9ZS6"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length){
+   firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
